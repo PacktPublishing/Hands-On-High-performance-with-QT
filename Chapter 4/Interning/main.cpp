22fc1f5af14ea0caa36ccd8b6350ce8fa19f6962
mrkkrj <marek@ib-krajewski.de>
2018-10-11 23:01:45 +0200

    addded the original N3599 proposal's code

--- Chapter 4/Interning/main.cpp
+++ Chapter 4/Interning/main.cpp
@@ -17,11 +17,31 @@ constexpr char chr(char const(&s)[N], int i) {
 #define intern_(s) Interned<chr(s,0),chr(s,1),chr(s,2),chr(s,3),chr(s,4),chr(s,5),chr(s,6)>::value
 
 #else
-// c++14 only
+// c++14 only, uses N3599 proposal
 template<typename CharT, CharT ...String> constexpr const char *operator""_intern() {
   static_assert(std::is_same<CharT, char>(), "can only intern narrow strings");
   return (Interned<String...>::value);
 }
+#endif
+
+#if 0
+// - original example from proposal N3599
+// this will compile with C++14, but lonk only with C++17!
+std::map<std::string, const char*> intern_map;
+template<char ...String> struct register_intern {
+  static constexpr char intern[] = { String..., 0 };
+  static register_intern register_;
+  register_intern() { intern_map[intern] = intern; }
+};
+template<char ...String> register_intern<String...> register_intern<String...>::register_;
+
+template<typename CharT, CharT ...String> constexpr const char *operator""_intern() {
+  static_assert(std::is_same<CharT, char>(), "can only intern narrow strings");
+  return (&register_intern<String...>::register_,
+          register_intern<String...>::intern);
+}
+
+static_assert("foo"_intern == "foo"_intern, "");
 #endif
 
 int main()
